b883ed9417f73228a47d351fdf2fcb2bd9b31534
Nathan Paul <nathanjpaul@gmail.com>
2019-04-23 21:31:52 -0500

    Simplify the conversion of collection date into speech

--- function.py
+++ function.py
@@ -15,35 +15,20 @@ import sanitation
 def convert_collection_date_to_speech(now, coll_date):
     days_until = (coll_date - now).days
 
-    # TODO: This nasty block needs some refactoring
     if days_until < 0:
         raise sanitation.UnknownCollectionDate()
     elif days_until == 0:
         return "today"
     elif days_until == 1:
-        if now.month == coll_date.month:
-            return "tomorrow"
-        elif now.year == coll_date.year:
-            return 'tomorrow, {}, <say-as interpret-as="date" format="md">{}</say-as>'.format(
-                coll_date.strftime("%A"), coll_date.strftime("%m/%d"))
-        else:
-            return 'tomorrow, {}, <say-as interpret-as="date" format="mdy">{}</say-as>'.format(
-                coll_date.strftime("%A"), coll_date.strftime("%m/%d/%Y"))
+        return "tomorrow"
     elif days_until < 7:
-        if now.month == coll_date.month:
-            return coll_date.strftime("%A")
-        elif now.year == coll_date.year:
-            return '{}, <say-as interpret-as="date" format="md">{}</say-as>'.format(
-                coll_date.strftime("%A"), coll_date.strftime("%m/%d"))
-        else:
-            return '{}, <say-as interpret-as="date" format="mdy">{}</say-as>'.format(
-                coll_date.strftime("%A"), coll_date.strftime("%m/%d/%Y"))
-    elif now.year != coll_date.year:
-        return '{}, <say-as interpret-as="date" format="mdy">{}</say-as>'.format(
-            coll_date.strftime("%A"), coll_date.strftime("%m/%d/%Y"))
-    else:
+        return coll_date.strftime("%A")
+    elif now.year == coll_date.year:
         return '{}, <say-as interpret-as="date" format="md">{}</say-as>'.format(
             coll_date.strftime("%A"), coll_date.strftime("%m/%d"))
+    else:
+        return '{}, <say-as interpret-as="date" format="mdy">{}</say-as>'.format(
+            coll_date.strftime("%A"), coll_date.strftime("%m/%d/%Y"))
 
 
 def create_skill_builder():
--- test_function.py
+++ test_function.py
@@ -6,74 +6,73 @@ from sanitation import UnknownCollectionDate
 
 class TestCollDateToSpeech(unittest.TestCase):
 
-    # TODO: Could do some more exhaustive testing here, making sure that
-    # all cases could be hit
-    def test_returns_today(self):
-        now = date(2019, 4, 22)
-        coll_date = date(2019, 4, 22)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(speech, 'today')
-
-    def test_returns_day_when_less_than_7_days_and_in_same_month(self):
-        now = date(2019, 4, 22)
-        coll_date = date(2019, 4, 28)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(speech, 'Sunday')
-
-    def test_returns_tomorrow_when_1_day_away_and_in_same_month(self):
-        now = date(2019, 4, 22)
-        coll_date = date(2019, 4, 23)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(speech, 'tomorrow')
-
-    def test_returns_day_and_month_when_at_least_7_days_and_in_same_month(self):
-        now = date(2019, 4, 22)
-        coll_date = date(2019, 4, 29)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(
-            speech,
-            'Monday, <say-as interpret-as="date" format="md">04/29</say-as>'
-        )
-
-    def test_returns_day_and_month_when_month_changes(self):
-        now = date(2019, 4, 29)
-        coll_date = date(2019, 5, 1)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(
-            speech,
-            'Wednesday, <say-as interpret-as="date" format="md">05/01</say-as>'
-        )
-
-    def test_returns_tomorrow_day_and_month_when_1_day_away_and_month_changes(self):
-        now = date(2019, 4, 30)
-        coll_date = date(2019, 5, 1)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(
-            speech,
-            'tomorrow, Wednesday, <say-as interpret-as="date" format="md">05/01</say-as>'
-        )
-
-    def test_returns_day_month_year_when_year_changes(self):
-        now = date(2019, 12, 30)
-        coll_date = date(2020, 1, 1)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(
-            speech,
-            'Wednesday, <say-as interpret-as="date" format="mdy">01/01/2020</say-as>'
-        )
-
-    def test_returns_tomorrow_day_month_year_when_year_changes(self):
-        now = date(2019, 12, 31)
-        coll_date = date(2020, 1, 1)
-        speech = convert_collection_date_to_speech(now, coll_date)
-        self.assertEqual(
-            speech,
-            'tomorrow, Wednesday, <say-as interpret-as="date" format="mdy">01/01/2020</say-as>'
-        )
-
     def test_raises_UnknownCollectionDate_when_in_past(self):
         now = date(2019, 4, 22)
         coll_date = date(2019, 4, 21)
 
         with self.assertRaises(UnknownCollectionDate):
             convert_collection_date_to_speech(now, coll_date)
+
+    def test_returns_today_when_0_days_away(self):
+        now = date(2019, 4, 22)
+        coll_date = date(2019, 4, 22)
+        speech = convert_collection_date_to_speech(now, coll_date)
+        self.assertEqual(speech, 'today')
+
+    def test_returns_tomorrow_when_1_day_away(self):
+        dates = [
+            # Same month
+            (date(2019, 4, 22), date(2019, 4, 23)),
+
+            # Different month, same year
+            (date(2019, 4, 30), date(2019, 5, 1)),
+
+            # Different year
+            (date(2019, 12, 31), date(2020, 1, 1)),
+        ]
+
+        for now, coll_date in dates:
+            with self.subTest(now=now, coll_date=coll_date):
+                speech = convert_collection_date_to_speech(now, coll_date)
+                self.assertEqual(speech, 'tomorrow')
+
+    def test_returns_day_when_less_than_7_days_away(self):
+        dates = [
+            # Same month
+            (date(2019, 4, 22), date(2019, 4, 24), "Wednesday"),
+
+            # Different month, same year
+            (date(2019, 4, 30), date(2019, 5, 2), "Thursday"),
+
+            # Different year
+            (date(2019, 12, 31), date(2020, 1, 3), "Friday"),
+        ]
+
+        for now, coll_date, exp in dates:
+            with self.subTest(now=now, coll_date=coll_date):
+                speech = convert_collection_date_to_speech(now, coll_date)
+                self.assertEqual(speech, exp)
+
+    def test_returns_day_and_month_when_at_least_7_days_away_and_same_year(self):
+        fmt = '{}, <say-as interpret-as="date" format="md">{}</say-as>'
+        dates = [
+            # Same month
+            (date(2019, 4, 22), date(2019, 4, 29), fmt.format("Monday", "04/29")),
+
+            # Different month, same year
+            (date(2019, 4, 22), date(2019, 5, 2), fmt.format("Thursday", "05/02")),
+        ]
+
+        for now, coll_date, exp in dates:
+            with self.subTest(now=now, coll_date=coll_date):
+                speech = convert_collection_date_to_speech(now, coll_date)
+                self.assertEqual(speech, exp)
+
+    def test_returns_day_month_year_when_at_least_7_days_away_and_year_changes(self):
+        now = date(2019, 12, 30)
+        coll_date = date(2020, 1, 6)
+        speech = convert_collection_date_to_speech(now, coll_date)
+        self.assertEqual(
+            speech,
+            'Monday, <say-as interpret-as="date" format="mdy">01/06/2020</say-as>'
+        )
